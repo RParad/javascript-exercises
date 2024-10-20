8069a6d492b7a69f7fcde4753232bc3ad4ff2cef
RParad <ryryps3@live.co.uk>
2024-10-20 16:02:46 +0100

    Completed exercises still WIP

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let removedReturn = array.filter(function (item) { 
+        return item != remove;
+    })
+    return removedReturn
 };
+// Failed example please look to the solution to understand why
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num, num2) {
+    if (!Number.isInteger(num) || !Number.isInteger(num2)) return "ERROR";
+    if (num < 0 || num2 < 0) return "ERROR";
+    if (num > num2) {
+      const temp = num;
+      num = num2;
+      num2 = temp;
+    }
+    
+    var sum = 0;
+    // Set a temporary variable i to start at value x.
+    // As long as the value of i is less than the value y, increment it.
+    // The loop will end when i is equal to y.
+    for (var i = num; i <= num2; i++) {
+      sum += i
+    }
+    return sum;
 
 };
 
